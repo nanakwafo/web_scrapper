5ab74dbfcba55f040e4ea91c53602bd422f34683
nanakwafo <nanamensah1140@gmail.com>
2022-02-25 18:19:17 +0000

    scrap a job site

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ import requests
 
 html_text=requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
 soup =BeautifulSoup(html_text,'lxml')
-jobs = soup.find('li',class_= 'clearfix job-bx wht-shd-bx')
-company_name = jobs.find('h3',class_='joblist-comp-name').text
-print(company_name)
+jobs = soup.find_all('li',class_= 'clearfix job-bx wht-shd-bx')
+for job in jobs:
+    published_date =job.find('span',class_ = 'sim-posted').text
+    if 'few' in published_date:
+        company_name = job.find('h3',class_='joblist-comp-name').text
+        skills = job.find('span',class_='srp-skills').text
+        
+        print(f'Company Name: {company_name.strip()}')
+        print(f'Required Skills: {skills.strip()}')
+        print(f'Published Date: {published_date.strip()}')
+        print('')
 # with open('home.html','r')  as  html_file:
 #     content = html_file.read()
 
