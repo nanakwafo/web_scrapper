87ce8a2f2a85c4e9194bfda5baaa88f40b1297ff
nanakwafo <nanamensah1140@gmail.com>
2022-02-25 14:58:00 +0000

    scrap a job site

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+from turtle import ht
 from bs4 import BeautifulSoup
+import requests 
 
-with open('home.html','r')  as  html_file:
-    content = html_file.read()
+html_text=requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&txtKeywords=python&txtLocation=').text
+soup =BeautifulSoup(html_text,'lxml')
+jobs = soup.find('li',class_= 'clearfix job-bx wht-shd-bx')
+company_name = jobs.find('h3',class_='joblist-comp-name').text
+print(company_name)
+# with open('home.html','r')  as  html_file:
+#     content = html_file.read()
 
-    soup =BeautifulSoup(content,'lxml')
-    course_cards = soup.find_all('div',class_= 'card')
-    for course in course_cards:
-        course_name =course.h5.text
-        course_price = course.a.text.split()[-1]
+#     soup =BeautifulSoup(content,'lxml')
+#     course_cards = soup.find_all('div',class_= 'card')
+#     for course in course_cards:
+#         course_name =course.h5.text
+#         course_price = course.a.text.split()[-1]
 
-        print(f'{course_name}  costs {course_price}')
+#         print(f'{course_name}  costs {course_price}')
