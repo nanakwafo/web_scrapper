2fcb887eba2c7e73f3630a24947941bfbffc3a2f
nanakwafo <nanamensah1140@gmail.com>
2022-02-25 13:06:15 +0000

    get courses and their prices

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ with open('home.html','r')  as  html_file:
     course_cards = soup.find_all('div',class_= 'card')
     for course in course_cards:
         course_name =course.h5.text
-        course_price = course.a.text
+        course_price = course.a.text.split()[-1]
 
-        print(course_name)
-        print(course_price)
+        print(f'{course_name}  costs {course_price}')
